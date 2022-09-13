4782f1e9f2faffb927bcb8088c2a98b7037847bb
MateusArenas <MateusArenas97@gmail.com>
2022-09-13 14:01:30 -0300

    all

--- api/index.php
+++ api/index.php
@@ -19,7 +19,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <?php include "/var/task/user/loaderStyleSheets.php";
+    <?php include "/var/task/user/api/..loaderStyleSheets.php";
         loaderStyleSheets();
     ?>
 
