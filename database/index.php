f43a625d56f1abb734d099c7c0f2d6c6f2c5ff16
MateusArenas <MateusArenas97@gmail.com>
2022-09-14 01:15:24 -0300

    all

--- database/index.php
+++ database/index.php
@@ -2,12 +2,14 @@
 
   $conn = new PDO('sqlite:/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
-  $conn->query('CREATE TABLE IF NOT EXISTS "db_users" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "name" VARCHAR,
-    "email" VARCHAR
-    -- "password" VARCHAR,
-    -- "time" DATETIME
-  )');
+  try {
+    $conn->query('CREATE TABLE IF NOT EXISTS "db_users" (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      "name" VARCHAR,
+      "email" VARCHAR
+    )');
+  } catch(\PDOException $e) {
+    die($e);
+  }
 
 ?>
