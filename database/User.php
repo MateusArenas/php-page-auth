8cedbbd7b4e63060bbe43b85093c983d4173f67c
MateusArenas <MateusArenas97@gmail.com>
2022-09-14 16:06:47 -0300

    new visual view 2s

--- database/User.php
+++ database/User.php
@@ -6,7 +6,7 @@
     public $id;
 
     public function __construct($fields) {
-        $this->conn = new PDO("sqlite:/tmp/db.sqlite");
+        $this->conn = new PDO("sqlite:".$_SERVER['DOCUMENT_ROOT']."/tmp/db.sqlite");
         $this->conn->query('CREATE TABLE IF NOT EXISTS "db_users" (
           "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
           "name" VARCHAR,
@@ -21,7 +21,7 @@
 
     static function find () {
       try {
-        $conn = new PDO("sqlite:/tmp/db.sqlite");
+        $conn = new PDO("sqlite:".$_SERVER['DOCUMENT_ROOT']."/tmp/db.sqlite");
         // $stmt = $this->conn->prepare('SELECT * FROM "db_users"');
 
         $stmt = $conn->query('SELECT * FROM "db_users" ORDER BY "created_at" ASC');
@@ -76,7 +76,7 @@
     }
 
     static function remove ($id) {
-      $conn = new PDO("sqlite:/tmp/db.sqlite");
+      $conn = new PDO("sqlite:".$_SERVER['DOCUMENT_ROOT']."/tmp/db.sqlite");
 
       $stmt = $conn->prepare("DELETE FROM db_users WHERE _id = :id");
     
