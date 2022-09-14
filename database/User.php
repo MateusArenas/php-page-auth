2afc66db0f7c9885845580425215f10edb414797
MateusArenas <MateusArenas97@gmail.com>
2022-09-14 11:49:30 -0300

    all

--- database/User.php
+++ database/User.php
@@ -1,19 +1,18 @@
 <?php 
 
+  $conn = new PDO('sqlite:/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+
+  $conn->query('CREATE TABLE IF NOT EXISTS "db_users" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "name" VARCHAR,
+    "email" VARCHAR
+  )');
+
   class User {
     public $fields;
     public $id;
-    public $conn;
 
     public function __construct($fields) {
-        $this->conn = new PDO('sqlite:/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
-
-        $this->conn->query('CREATE TABLE IF NOT EXISTS "db_users" (
-          "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-          "name" VARCHAR,
-          "email" VARCHAR
-        )');
-
         $this->fields = [
           "name" => $fields["name"],
           "email" => $fields["email"]
@@ -22,7 +21,7 @@
 
     static function find () {
       try {
-        $conn = new PDO('sqlite:/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+        global $conn;
         // $stmt = $this->conn->prepare('SELECT * FROM "db_users"');
 
         $stmt = $conn->query('SELECT * FROM "db_users"');
@@ -41,13 +40,15 @@
   
     public function save () {
       try {
-        $stmt = $this->conn->prepare("INSERT INTO db_users (name, email) VALUES(:name, :email)");
+        global $conn;
+
+        $stmt = $conn->prepare("INSERT INTO db_users (name, email) VALUES(:name, :email)");
       
         foreach ($this->fields as $key => &$val) $stmt->bindParam($key, $val);
 
         $stmt->execute();
 
-        $this->id = (int)$this->conn->lastInsertId();
+        $this->id = (int)$conn->lastInsertId();
         // $this->id = (int)SQLite3::lastInsertRowID();
       } catch(\PDOException $e) {
         die($e);
@@ -55,7 +56,9 @@
     }
 
     public function update ($update) {
-      $stmt = $this->conn->prepare("UPDATE db_users SET name = :name, email = :email WHERE _id = :id");
+      global $conn;
+
+      $stmt = $conn->prepare("UPDATE db_users SET name = :name, email = :email WHERE _id = :id");
     
       $this->changeFields($update);
 
@@ -67,7 +70,9 @@
     }
 
     public function delete () {
-      $stmt = $this->conn->prepare("DELETE FROM db_users WHERE _id = :id");
+      global $conn;
+
+      $stmt = $conn->prepare("DELETE FROM db_users WHERE _id = :id");
     
       $stmt->bindParam("id", $this->id);
 
@@ -75,8 +80,8 @@
     }
 
     static function remove ($id) {
-      $conn = new PDO('sqlite:/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
-
+      global $conn;
+      
       $stmt = $conn->prepare("DELETE FROM db_users WHERE _id = :id");
     
       $stmt->bindParam("id", $id);
