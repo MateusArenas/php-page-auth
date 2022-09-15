1381797ed71784ed9269bc4d942eae688d12d73a
MateusArenas <MateusArenas97@gmail.com>
2022-09-15 14:33:15 -0300

    add images file exists

--- components/HeaderMaster.php
+++ components/HeaderMaster.php
@@ -8,14 +8,14 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse collapse" id="navbarToggleExternalContent">
-                <div class="me-auto navbar-nav">
-                    <a href="dashboard.php" data-rr-ui-event-key="#dashboard" class="nav-link">dashboard</a>
-                    <!-- <a href="#pricing" data-rr-ui-event-key="#pricing" class="nav-link">Pricing</a> -->
-                    <!-- <div class="nav-item dropdown"><a id="collasible-nav-dropdown" aria-expanded="false" role="button" class="dropdown-toggle nav-link" tabindex="0" href="#">Dropdown</a></div> -->
-                </div>
-                <div class="navbar-nav"><a href="signin.php" data-rr-ui-event-key="#deets" class="nav-link me-2">Sign In</a>
-                <a href="signup.php" data-rr-ui-event-key="2" class="btn btn-outline-light">Sign Up</a></div>
+                <div class="navbar-collapse collapse mt-2 mt-lg-0" id="navbarToggleExternalContent">
+                    <div class="me-auto navbar-nav">
+                        <a href="dashboard.php" data-rr-ui-event-key="#dashboard" class="nav-link">dashboard</a>
+                        <!-- <a href="#pricing" data-rr-ui-event-key="#pricing" class="nav-link">Pricing</a> -->
+                        <!-- <div class="nav-item dropdown"><a id="collasible-nav-dropdown" aria-expanded="false" role="button" class="dropdown-toggle nav-link" tabindex="0" href="#">Dropdown</a></div> -->
+                    </div>
+                    <div class="navbar-nav"><a href="signin.php" data-rr-ui-event-key="#deets" class="nav-link me-2">Sign In</a>
+                    <a href="signup.php" data-rr-ui-event-key="2" class="btn my-2 my-lg-0 btn-outline-light">Sign Up</a></div>
                 </div>
             </div>
             <hr class="w-100 bg-secondary opacity-10 m-0 position-absolute bottom-0">
