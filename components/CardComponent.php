27e73563f82861ebd292f832ad6407a97f38d5fb
MateusArenas <MateusArenas97@gmail.com>
2022-09-15 14:48:38 -0300

    add images file exists

--- components/CardComponent.php
+++ components/CardComponent.php
@@ -9,7 +9,7 @@
   }
     
 ?>
-  <a href="<?php echo "/signin.php?email=".$props["email"]."&"."password=".$props["password"]; ?>" class="list-group-item list-group-item-action text-white <?php echo $props['active'] ? "active" : "bg-dark" ?> <?php echo $props['disabled'] ? "disabled" : "" ?>">
+  <a href="<?php echo "signin.php?email=".$props["email"]."&"."password=".$props["password"]; ?>" class="list-group-item list-group-item-action text-white <?php echo $props['active'] ? "active" : "bg-dark" ?> <?php echo $props['disabled'] ? "disabled" : "" ?>">
     <div class="d-flex flex-row w-100 py-2 pe-1">
       
       <div style="width: 62px; height: 62px;" class="badge me-3 text-uppercase d-flex align-items-center justify-content-center fw-bold text-center bg-dark text-primary border border-primary rounded-circle fs-2">
